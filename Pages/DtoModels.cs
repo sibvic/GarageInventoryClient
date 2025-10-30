3408e7e353b45582628410594e705e09ae3661df
Victor Tereschenko <sibvic@gmail.com>
2025-10-30 20:44:00 +0600

    feat: Edit projects

--- Pages/DtoModels.cs
+++ Pages/DtoModels.cs
@@ -5,6 +5,7 @@ public class ProjectDto
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public DateTime CreationDate { get; set; }
+    public decimal? Price { get; set; }
 }
 
 public class ItemDto
